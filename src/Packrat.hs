caad410f8de42b3dc97c75eba8d1cd368719256e
Phantomas <phantomas@phantomas.xyz>
2020-09-20 16:32:46 +0200

    [ADD] Add Packrat main module.

---
+++ src/Packrat.hs
@@ -0,0 +1,13 @@
+module Packrat (
+  module Packrat.Prim,
+  module Packrat.Positions,
+  module Packrat.Errors,
+  module Packrat.Combinators,
+  module Packrat.Char
+  ) where
+
+import Packrat.Prim
+import Packrat.Positions
+import Packrat.Errors
+import Packrat.Combinators
+import Packrat.Char
