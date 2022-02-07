69f5ab0fa9637fd9b0465bfbf9e71b90bee16202
Phantomas <phantomas@phantomas.xyz>
2022-02-07 01:08:50 +0100

    feat: tab_width handling for PackPos

--- src/Packrat/Positions.hs
+++ src/Packrat/Positions.hs
@@ -3,24 +3,28 @@ module Packrat.Positions where
 data PackPos = PackPos {
               filename :: String,
               line :: Int,
-              column :: Int
+              column :: Int,
+              tab_length :: Int
 }
 
-newpos :: String -> PackPos
-newpos file = PackPos file 1 1
+newPos :: String -> PackPos
+newPos file = PackPos file 1 1 4
 
-nextpos :: PackPos -> Char -> PackPos
-nextpos (PackPos f l c) '\n' = PackPos f (l + 1) c
-nextpos (PackPos f l c) '\t' = PackPos f l (c + 4)
-nextpos (PackPos f l c) _ = PackPos f l (c + 1)
+newPosTab :: String -> Int -> PackPos
+newPosTab file = PackPos file 1 1 
+
+nextPos :: PackPos -> Char -> PackPos
+nextPos pos@(PackPos _ l c _) '\n' = pos {line=l + 1}
+nextPos pos@(PackPos _ _ c le) '\t' = pos {column = c + le}
+nextPos pos@(PackPos _ _ c _) _ = pos {column = c + 1}
 
 instance Eq PackPos where
-  (PackPos fl ll cl) == (PackPos fr lr cr) =
+  (PackPos fl ll cl _) == (PackPos fr lr cr _) =
     fl == fr && ll == lr && cl == cr
 
 instance Ord PackPos where
-  (PackPos fl ll cl) <= (PackPos fr lr cr) =
+  (PackPos fl ll cl _) <= (PackPos fr lr cr _) =
     (ll < lr) || (ll == lr && cl <= cr)
 
 instance Show PackPos where
-  show (PackPos f l c) = show f ++ ":" ++ show l ++ ":" ++ show c ++ ": "
+  show (PackPos f l c _) = show f ++ ":" ++ show l ++ ":" ++ show c ++ ": "
