1c4d2e1b5274ead82c00bfc42df9f1e5232847dc
Phantomas <phantomas@phantomas.xyz>
2022-02-13 14:29:14 +0100

    ref: make String.oneOf strict by default
    There barely any need for a non strict oneOf, and oneOf' broke haskell-language-server somehow

--- src/Packrat/String.hs
+++ src/Packrat/String.hs
@@ -1,7 +1,6 @@
 module Packrat.String (
   string
   , oneOf
-  , oneOf'
 ) where
 
 import Control.Applicative ((<|>))
@@ -16,15 +15,9 @@ string :: Derivs d => String -> Parser d String
 string s = foldr (\c -> (<*>) ((:) <$> char c)) (pure []) s <?> show s
 
 oneOf :: Derivs d => [String] -> Parser d String
-oneOf [] = Parser p 
-  where p d = NoParse $ internalError d "String.oneOf was called without any strings"
-oneOf [str] = string str
-oneOf strs@(s:snxt) = (string s <|> oneOf snxt) <?> ("one string of: " ++ unwords strs)
-
-oneOf' :: Derivs d => [String] -> Parser d String
-oneOf' [] = Parser p
+oneOf [] = Parser p
   where p d = NoParse $ internalError d "String.oneOf' was called without any strings"
-oneOf' strs = sub (sortOn (negate . length) strs) <?> ("one string of: " ++ unwords strs)
+oneOf strs = sub (sortOn (negate . length) strs) <?> ("one string of: " ++ unwords strs)
   where sub [] = undefined
         sub [str] = string str
         sub strs@(s:nxt) = string s <|> sub nxt
