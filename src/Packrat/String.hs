011cf94036cd3537a99e78792633bc3f6384ba70
Phantomas <phantomas@phantomas.xyz>
2022-02-13 12:30:25 +0100

    add: add strict version of String.oneOf

--- src/Packrat/String.hs
+++ src/Packrat/String.hs
@@ -1,9 +1,12 @@
 module Packrat.String (
-  string,
-  oneOf
+  string
+  , oneOf
+  , oneOf'
 ) where
 
 import Control.Applicative ((<|>))
+
+import Data.List
 
 import Packrat.Prim
 import Packrat.Errors
@@ -14,6 +17,14 @@ string s = foldr (\c -> (<*>) ((:) <$> char c)) (pure []) s <?> show s
 
 oneOf :: Derivs d => [String] -> Parser d String
 oneOf [] = Parser p 
-  where p d = NoParse $ internalError d "oneStringOf was called without any strings"
+  where p d = NoParse $ internalError d "String.oneOf was called without any strings"
 oneOf [str] = string str
 oneOf strs@(s:snxt) = (string s <|> oneOf snxt) <?> ("one string of: " ++ unwords strs)
+
+oneOf' :: Derivs d => [String] -> Parser d String
+oneOf' [] = Parser p
+  where p d = NoParse $ internalError d "String.oneOf' was called without any strings"
+oneOf' strs = sub (sortOn (negate . length) strs) <?> ("one string of: " ++ unwords strs)
+  where sub [] = undefined
+        sub [str] = string str
+        sub strs@(s:nxt) = string s <|> sub nxt
